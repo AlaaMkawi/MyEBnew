84eb8cef44b8a5313e553146be8d7636d294ab96
alaa mkawi <alaamk@ac.sce.ac.il>
2024-03-08 02:45:06 +0200

    log in pys

--- account/tests.py
+++ account/tests.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User, Group
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
 from django.contrib.auth import login
 from account.views import loginpediatrician,loginpsycholist
 import unittest
-from djang.contrib.auth.models import User, Group
+
 
 
 class Testloginpediatrician(TestCase):
@@ -26,6 +27,26 @@ class Testloginpediatrician(TestCase):
     def Testnothingusernamepasswordloginped(self):
         client = Client()
         response = client.post('loginpediatrician/', {'username': 'invalid', 'password': 'wrong password'})
+        self.assertEqual(response.status_code, 'username and password incorrect')
+class Testloginpsychologist(TestCase):
+    def testcorrecrloginpsy(self):
+        client = Client()
+        response = client.post('loginpsycholist//', {'username':'testuser', 'password':'<PASSWORD>'})
+        self.assertEqual(response.status_code, 200)
+
+    def Testwrongusernameloginpsy(self):
+        client = Client()
+        response = client.post('loginpsycholist//', {'username': 'invalid', 'password': '<PASSWORD>'})
+        self.assertEqual(response.status_code, 'username incorrect')
+
+    def Testwrongpasswordloginpsy(self):
+        client = Client()
+        response = client.post('loginpsycholist//', {'username': 'testuser', 'password': 'wrong password'})
+        self.assertEqual(response.status_code, 'password incorrect')
+
+    def Testnothingusernamepasswordloginpsy(self):
+        client = Client()
+        response = client.post('loginpsycholist//', {'username': 'invalid', 'password': 'wrong password'})
         self.assertEqual(response.status_code, 'username and password incorrect')
 
 
